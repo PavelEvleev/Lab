b0b326ad62c65cbc6a8a4fe9596ea32881491894
Pavel Evleev <mr.malefiic@gmail.com>
2017-03-31 10:12:28 +0300

    first commit

---
+++ SumTwoTextBox/SumTwoTextBox/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SumTwoTextBox.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="x1" runat="server" Width="110px"></asp:TextBox>
+&nbsp;+
+        <asp:TextBox ID="x2" runat="server" Width="153px"></asp:TextBox>
+&nbsp;=
+        <asp:Label ID="ResultSum" runat="server" Text="Result"></asp:Label>
+&nbsp;<br />
+        <br />
+        <asp:Button ID="SumButton" runat="server" Text="Sum" OnClick="SumButton_Click" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ SumTwoTextBox/SumTwoTextBox/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SumTwoTextBox
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                x1.Text = "0";
+                x2.Text = "0";
+            }
+        }
+
+        protected void SumButton_Click(object sender, EventArgs e)
+        {
+            int first = Convert.ToInt32(x1.Text);
+            int second = Convert.ToInt32(x2.Text);
+            ResultSum.Text = Convert.ToString(first + second);
+        }
+    }
+}
